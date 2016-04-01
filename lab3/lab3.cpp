1cf014bdb9ebaf45a315fcb831483ffff61fbf1a
Eric Peter Stavarache <ericptst@gmail.com>
2016-04-02 00:16:39 +0300

    Added lab3

---
+++ lab3/lab3.cpp
@@ -0,0 +1,89 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <string>
+#include <utility>
+
+#define end(x, y) (make_pair(x,  y))
+
+using std::string;
+using std::pair;
+using std::make_pair;
+
+const char lambda = 'L';
+
+
+struct node {
+  static int qAt;
+  int q;
+  std::unordered_map<char, node*> T;
+
+  node() {
+    q = qAt;
+    qAt++;
+  }
+
+  node* addTransition(char c, node* n) {
+    T[c] = n;
+    return n;
+  }
+
+  node* addTransition(char c) {
+    node* add = new node();
+    return addTransition(c, add);
+  }
+};
+
+typedef pair<node*, node*> pnn;
+
+int node::qAt = 0;
+
+string s;
+
+pnn parse() {
+//returns the start node and the end node
+  static int i = 0;
+  node* st = new node();
+  node* now = st;
+
+  while(i < s.length()) {
+    if(s[i] == '(') {
+      i++;
+      pnn next = parse();
+      now->addTransition(lambda, next.first);
+      now = next.second;
+    } else 
+    if(s[i] == ')') {
+      i++;
+      return end(st, now);
+    } else
+    if(std::isalpha(s[i])) {
+      now = now->addTransition(s[i]);
+      i++;
+    }
+  } 
+
+  return end(st, now);
+}
+
+std::unordered_set<int> visited;
+
+void show(node* st) {
+  visited.insert(st->q);
+  for(auto i: st->T) {
+    std::cout << st->q << " + " << i.first << " => " << i.second->q << "\n";
+  }
+  for(auto i: st->T) {
+    if(!visited.count(i.second->q)) {
+      show(i.second);
+    }
+  }
+}
+
+int main() {
+  std::cin >> s;
+  pnn automat = parse();
+  show(automat.first);
+  std::cout << "start => " << automat.first->q << "\nfinal => " << automat.second->q << "\n";
+  return 0;
+}
