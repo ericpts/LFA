7429b8dcedd83d815873f239b888d12e5a21df6e
Eric Peter Stavarache <ericptst@gmail.com>
2016-04-02 08:09:08 +0300

    Added support for | and *

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <vector>
 #include <unordered_set>
 #include <iostream>
 #include <string>
@@ -6,6 +7,7 @@
 
 #define end(x, y) (make_pair(x,  y))
 
+using std::cerr;
 using std::string;
 using std::pair;
 using std::make_pair;
@@ -16,7 +18,7 @@ const char lambda = 'L';
 struct node {
   static int qAt;
   int q;
-  std::unordered_map<char, node*> T;
+  std::unordered_multimap<char, node*> T;
 
   node() {
     q = qAt;
@@ -24,7 +26,8 @@ struct node {
   }
 
   node* addTransition(char c, node* n) {
-    T[c] = n;
+    cerr << "connected " << q << " + " << c << " => " << n->q <<  "\n";
+    T.insert(make_pair(c, n));
     return n;
   }
 
@@ -40,9 +43,21 @@ int node::qAt = 0;
 
 string s;
 
-pnn parse() {
-//returns the start node and the end node
-  static int i = 0;
+int i = 0;
+
+pnn parse();
+pnn concat();
+
+pnn cycle(node* st, node* fin) {
+  fin->addTransition(lambda, st);
+  i++;
+  return make_pair(st, fin);
+}
+
+pnn concat() {
+
+  cerr << "entering concat\n";
+
   node* st = new node();
   node* now = st;
 
@@ -54,16 +69,79 @@ pnn parse() {
       now = next.second;
     } else 
     if(s[i] == ')') {
-      i++;
+      cerr << "ending concat because of )\n";
       return end(st, now);
     } else
     if(std::isalpha(s[i])) {
       now = now->addTransition(s[i]);
       i++;
+    } else
+    if(s[i] == '|') {
+      cerr << "end concat because of |\n";
+      return end(st, now);
+    } else
+    if(s[i] == '*') {
+      now->addTransition(lambda, st);
+      now = st;
+      i++;
+    }
+  }
+
+  cerr << "end concat\n";
+  return end(st, now);
+}
+
+pnn parse() {
+//returns the start node and the end node
+  std::vector<pnn> nxt;
+  cerr << "entering parse\n";
+
+  while(i < s.length()) {
+    if(s[i] == ')') {
+      cerr << "found )\n";
+      i++;
+      break;
+    } else
+    if(s[i] == '(') {
+      cerr << "found (\n";
+      i++;
+      nxt.push_back(parse());
+    } else
+    if(s[i] == '|') {
+      cerr << "found |\n";
+      i++;
+      nxt.push_back(concat());
+    } else
+    if(s[i] == '*') {
+      cerr << "found * in parse\n";
+      nxt.back().second->addTransition(lambda, nxt.back().first);
+      nxt.back().second = nxt.back().first;
+      i++;
+    } else {
+      pnn next = concat();
+      cerr << "extending nxt with concat\n";
+      if(!nxt.empty()) {
+        nxt.back().second->addTransition(lambda, next.first);
+        nxt.back().second = next.second;
+      } else {
+        nxt.push_back(next);
+      }
     }
   } 
 
-  return end(st, now);
+  node* st = new node();
+  node* fin = new node();
+
+  cerr << "nxt has size " << nxt.size() << "\n";
+
+  for(auto i: nxt) {
+    st->addTransition(lambda, i.first);
+    i.second->addTransition(lambda, fin);
+  }
+
+  cerr << "return from parse\n";
+
+  return make_pair(st, fin);
 }
 
 std::unordered_set<int> visited;
